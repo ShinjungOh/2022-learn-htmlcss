ca223d20b9420daacddc6eed500f1716e176a503
ShinjungOh <tlswjd9260@gmail.com>
2022-01-21 20:04:38 +0900

    [ADD] 유노코딩 자바스크립트 출력문

--- 2022-01-20/script.js
+++ 2022-01-20/script.js
@@ -3,3 +3,19 @@ console.log(0);
 console.log(3.14);
 console.log("")
 console.log('112   334dsf')
+console.clear();
+
+let aaa = 5;
+console.log(aaa);
+aaa = 6;
+console.log(aaa);
+
+const bb = 2;
+console.log(bb);
+// bb = 7;
+console.log(bb);
+
+
+const promptMessage = "프롬프트에 표시할 문자열";
+const result = prompt(promptMessage)
+console.log(result)
